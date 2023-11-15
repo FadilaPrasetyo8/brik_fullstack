59d683bb72b759a9577e34c385c5ce4592f169b0
FadilaPrasetyo8 <fadilaprasetyo8@gmail.com>
2023-11-15 14:37:52 +0700

    fix: protect router login

--- client/src/require-sidebar/index.jsx
+++ client/src/require-sidebar/index.jsx
@@ -1,10 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
+import { UseLocalStorage } from "../hooks/use-local-storage";
 import { Sidebar } from "../components/ui/sidebar";
+import { useEffect, useState } from "react";
 
 export const RequireSidebar = () => {
-  return (
+  const { getItem } = UseLocalStorage();
+  const navigate = Navigate();
+  const [getToken, setGetToken] = useState("");
+
+  useEffect(() => {
+    const token = getItem("token");
+    setGetToken(token);
+  }, []);
+
+  let auth = getToken;
+
+  return !auth.token ? (
     <Sidebar>
       <Outlet />
     </Sidebar>
+  ) : (
+    <Navigate to="/" replace={true} />
   );
 };
